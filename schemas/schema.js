520c03d941fbf6725a912607ec72966cd63776bd
Francisco Garciarena <francisco.garciarena94@gmail.com>
2022-08-11 17:57:57 -0300

    hero img

--- schemas/schema.js
+++ schemas/schema.js
@@ -3,8 +3,9 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import eventos from './eventos'
 import temas from './temas'
 import artistas from './artistas'
+import hero from './hero'
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([temas, artistas, eventos]),
+  types: schemaTypes.concat([temas, artistas, eventos, hero]),
 })
